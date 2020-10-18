4c7711681d60d7946f5f91adbb90dde56eb1c383
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2020-10-17 22:42:53 -0400

    Add files via upload

--- PyBANK_SJ/main.py
+++ PyBANK_SJ/main.py
@@ -19,4 +19,25 @@ with open(csvpath) as csvfile:
         Average_Change.append(row[3])
         Greatest_Profit_Increase.append(row[4])
         Greatest_Profit_Decrease.append(row[5])
+    
+#For total months
+    for total_months in months:
+        totalmonths = total + 1
+#For net total
+    for total in data:
+        total = total+data[1]
+#For avergae change
+    for average in data:
+        average = total/totalmonths
+#For greatest increase
+    total.sort()
+    max = total[-1]
+#for greatest decrease
+    min = total[0]
 
+print (totalmonths0
+print (total)
+print (average)
+print (max)
+print (min)
+  
